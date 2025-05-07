a035a4c197ab4ba8db5e74570ca48560cbac3266
William Cairns <cairnswm@gmail.com>
2025-05-07 16:10:04 +0200

    Add error reporting

--- php/api/api.php
+++ php/api/api.php
@@ -1,6 +1,11 @@
 <?php
 
 include_once dirname(__FILE__) . "/../corsheaders.php";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include_once dirname(__FILE__) . "/../gapiv2/dbconn.php";
 include_once dirname(__FILE__) . "/../gapiv2/v2apicore.php";
 include_once dirname(__FILE__) . "/../utils.php";
